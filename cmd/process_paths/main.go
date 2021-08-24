053f2021a9768443606310bfd8e69cf3aef99246
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2021-08-24 23:21:46 +0400

    add output type flag

--- cmd/process_paths/main.go
+++ cmd/process_paths/main.go
@@ -1,27 +1,38 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/nij4t/vt-process-scanner/pkg/process"
 )
 
+var outType = flag.String("out-type", "path", "Output type (path | hash)")
+
 func main() {
-	// taskList, err := getProcessList()
 
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	flag.Parse()
 
-	// proc, err := getProcessPath(taskList[0])
 	proc, err := process.GetProcessPaths()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// fmt.Println(strings.Join(taskList, "\n"))
+	if *outType == "hash" {
+		for _, v := range proc {
+			hash := sha256.Sum256([]byte(v))
+			encoder := hex.NewEncoder(os.Stdout)
+			encoder.Write(hash[:])
+			fmt.Println()
+		}
+		os.Exit(0)
+	}
+
 	fmt.Println(strings.Join(proc, "\n"))
 }
