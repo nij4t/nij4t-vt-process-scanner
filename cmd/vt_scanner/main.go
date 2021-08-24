5a16fa756f0175cd7b48721fafa573aa31767056
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2021-08-24 21:31:11 +0400

    add hashlist file support

--- cmd/vt_scanner/main.go
+++ cmd/vt_scanner/main.go
@@ -6,58 +6,91 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 
 	vt "github.com/VirusTotal/vt-go"
 )
 
 var apikey = flag.String("apikey", "", "VirusTotal API key")
 var sha256 = flag.String("sha256", "", "SHA-256 of some file")
+var hashList = flag.String("hashlist", "", "File containing hashes")
 var out = flag.String("out", "", "Output file")
 
 func main() {
 
+	hashes := []string{}
+
 	flag.Parse()
 
-	if *apikey == "" || *sha256 == "" {
-		fmt.Println("Must pass both the --apikey and --sha256 arguments.")
-		os.Exit(0)
+	if *apikey == "" {
+		fmt.Println("Must pass --apikey argument.")
+		os.Exit(1)
+	}
+
+	if *hashList != "" {
+		fileWithHashes, err := os.ReadFile(*hashList)
+		if err != nil {
+			log.Fatal(err)
+		}
+		hashes = strings.Split(string(fileWithHashes), "\n")
+	}
+
+	if *sha256 != "" {
+		hashes = append(hashes, *sha256)
 	}
 
 	client := vt.NewClient(*apikey)
 
-	file, err := client.GetObject(vt.URL("files/%s", *sha256))
-	if err != nil {
-		log.Fatal(err)
+	reports := []*vt.Object{}
+	distributors := []string{}
+
+	for _, hash := range hashes {
+		report, err := getReport(client, hash)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		reports = append(reports, report)
+		ls, err := report.GetTime("last_submission_date")
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Printf("File %s was submitted for the last time on %v\n", report.ID(), ls)
+
+		distributor, err := getDistributors(report)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		if distributor, ok := distributor.([]string); ok {
+			distributors = append(distributors, distributor[0])
+		}
 	}
 
-	ls, err := file.GetTime("last_submission_date")
-	if err != nil {
-		log.Fatal(err)
+	output := os.Stdout
+
+	if *out != "" {
+		output, err := os.OpenFile(*out, os.O_WRONLY|os.O_CREATE, 0644)
+		defer output.Close()
+
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	fmt.Printf("File %s was submitted for the last time on %v\n", file.ID(), ls)
-
-	data, err := file.MarshalJSON()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// fmt.Println(string(data))
-
-	if *out == "" {
-		os.Exit(0)
-	}
-
-	f, err := os.OpenFile(*out, os.O_WRONLY|os.O_CREATE, 0644)
-	defer f.Close()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = f.Write(data)
+	// _, err = f.Write(data)
+	_, err := output.Write([]byte(strings.Join(distributors, "\n")))
 
 	if err != nil && err != io.EOF {
 		log.Fatal(err)
 	}
+}
+
+func getReport(client *vt.Client, hash string) (*vt.Object, error) {
+	return client.GetObject(vt.URL("files/%s", *sha256))
+}
+
+func getDistributors(report *vt.Object) (interface{}, error) {
+	return report.Get("distributors")
 }
